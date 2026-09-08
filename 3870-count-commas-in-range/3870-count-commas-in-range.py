7191e6627d5eb3aa9c21d04c8f15f87f8dd7b703
Taran Mamidala <mamidalataran6333@gmail.com>
2026-09-07 20:31:43 -0400

    Solves 3870. Count Commas in Range - Easy (Accepted)

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -0,0 +1,9 @@
+class Solution:
+
+    def countCommas(self, n: int) -> int:
+        total_commas = 0
+        threshold = 1000
+        while threshold <= n:
+            total_commas += n - threshold + 1
+            threshold *= 1000
+        return total_commas
