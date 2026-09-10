344ff8c3341d6cc173d9681facea12b36ffaf1c2
Taran Mamidala <mamidalataran6333@gmail.com>
2026-09-10 09:01:18 -0400

    Solves 2265. Count Nodes Equal to Average of Subtree - Medium (Accepted)

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
@@ -1,18 +1,17 @@
 class Solution:
 
     def averageOfSubtree(self, root: 'TreeNode') -> int:
-        matching_nodes_count = 0
+        self.matching_nodes_count = 0
 
-        def dfs(node) -> tuple:
-            nonlocal matching_nodes_count
+        def calculate_subtree_stats(node):
             if not node:
                 return (0, 0)
-            left_sum, left_count = dfs(node.left)
-            right_sum, right_count = dfs(node.right)
-            curr_sum = left_sum + right_sum + node.val
-            curr_count = left_count + right_count + 1
-            if curr_sum // curr_count == node.val:
-                matching_nodes_count += 1
-            return (curr_sum, curr_count)
-        dfs(root)
-        return matching_nodes_count
+            left_sum, left_count = calculate_subtree_stats(node.left)
+            right_sum, right_count = calculate_subtree_stats(node.right)
+            total_sum = left_sum + right_sum + node.val
+            total_count = left_count + right_count + 1
+            if total_sum // total_count == node.val:
+                self.matching_nodes_count += 1
+            return (total_sum, total_count)
+        calculate_subtree_stats(root)
+        return self.matching_nodes_count
