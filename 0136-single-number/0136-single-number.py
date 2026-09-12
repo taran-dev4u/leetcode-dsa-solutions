0b1cba9a0b7f657db6da9cda105f14d779a0776c
Taran Mamidala <mamidalataran6333@gmail.com>
2026-09-12 13:54:16 -0400

    Solves 136. Single Number - Easy (Accepted)

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,8 @@
+from typing import List
+import functools
+import operator
+
+class Solution:
+
+    def singleNumber(self, nums: List[int]) -> int:
+        return functools.reduce(operator.xor, nums)
