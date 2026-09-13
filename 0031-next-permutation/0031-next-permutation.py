28f66a926003569b08f808b5407b41fc7bd400a0
Taran Mamidala <mamidalataran6333@gmail.com>
2026-09-13 11:32:00 -0400

    Solves 31. Next Permutation - Medium (Accepted)

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+
+    def nextPermutation(self, nums: List[int]) -> None:
+        n = len(nums)
+        if n <= 1:
+            return
+        i = n - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        if i >= 0:
+            j = n - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = (nums[j], nums[i])
+        left, right = (i + 1, n - 1)
+        while left < right:
+            nums[left], nums[right] = (nums[right], nums[left])
+            left += 1
+            right -= 1
