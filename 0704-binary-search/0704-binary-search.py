87cd4a86f38062a705cbb822464757aed984a9c0
Taran Mamidala <mamidalataran6333@gmail.com>
2026-09-14 22:33:54 -0400

    Solves 704. Binary Search - Easy (Accepted)

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = (0, len(nums) - 1)
+        while left <= right:
+            mid = left + (right - left) // 2
+            mid_val = nums[mid]
+            if mid_val == target:
+                return mid
+            elif mid_val < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
