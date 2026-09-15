1854f027bbc1a1356c975a05d629a9f01159e86a
Taran Mamidala <mamidalataran6333@gmail.com>
2026-09-15 17:04:36 -0400

    Solves 540. Single Element in a Sorted Array - Medium (Accepted)

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        low = 0
+        high = len(nums) - 1
+        while low < high:
+            mid = (low + high) // 2
+            if nums[mid] == nums[mid ^ 1]:
+                low = mid + 1
+            else:
+                high = mid
+        return nums[low]
