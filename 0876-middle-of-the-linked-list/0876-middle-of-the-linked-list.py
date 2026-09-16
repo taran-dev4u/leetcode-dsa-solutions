a33b5511e960301eeba108729733c2b221b3bbfb
Taran Mamidala <mamidalataran6333@gmail.com>
2026-09-16 15:37:59 -0400

    Solves 876. Middle of the Linked List - Easy (Accepted)

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -0,0 +1,8 @@
+class Solution:
+
+    def middleNode(self, head: ListNode | None) -> ListNode | None:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
