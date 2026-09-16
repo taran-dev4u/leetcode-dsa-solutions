eadde1e273e6e6e81aeb15c8920fa6caae3b2224
Taran Mamidala <mamidalataran6333@gmail.com>
2026-09-16 15:38:33 -0400

    Solves 206. Reverse Linked List - Easy (Accepted)

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def reverseList(self, head: ListNode | None) -> ListNode | None:
+        prev = None
+        curr = head
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        return prev
