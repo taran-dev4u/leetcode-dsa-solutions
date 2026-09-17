d3d0278e4e2e34d7cd88425606261c028868b179
Taran Mamidala <mamidalataran6333@gmail.com>
2026-09-17 11:31:26 -0400

    Solves 14. Longest Common Prefix - Easy (Accepted)

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,9 +3,10 @@ class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
         if not strs:
             return ''
-        s1 = min(strs)
-        s2 = max(strs)
-        for i, char in enumerate(s1):
-            if i >= len(s2) or char != s2[i]:
-                return s1[:i]
-        return s1
+        prefix = []
+        for chars in zip(*strs):
+            if len(set(chars)) == 1:
+                prefix.append(chars[0])
+            else:
+                break
+        return ''.join(prefix)
