03558f7609d482327dc250ccd37cab31061d252d
Taran Mamidala <mamidalataran6333@gmail.com>
2026-09-22 16:53:32 -0400

    Solves 204. Count Primes - Medium (Accepted)

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def countPrimes(self, n: int) -> int:
+        if n <= 2:
+            return 0
+        is_prime = bytearray([1]) * n
+        is_prime[0] = is_prime[1] = 0
+        for i in range(2, int(n ** 0.5) + 1):
+            if is_prime[i]:
+                is_prime[i * i:n:i] = bytearray(len(range(i * i, n, i)))
+        return sum(is_prime)
