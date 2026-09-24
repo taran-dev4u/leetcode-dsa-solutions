58318855aafabbf3f6a92e98816a75d8ce059948
Taran Mamidala <mamidalataran6333@gmail.com>
2026-09-24 15:03:04 -0400

    Solves 155. Min Stack - Medium (Accepted)

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,19 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value: int) -> None:
+        if not self.stack:
+            self.stack.append((value, value))
+        else:
+            self.stack.append((value, min(value, self.stack[-1][1])))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
